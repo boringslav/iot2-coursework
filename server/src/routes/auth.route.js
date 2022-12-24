6150ec7524c5f40979d6e7bdd3d33e3fb71ca894
Borislav Stoyanov <borislav.stoyanov@musala.com>
2022-12-24 14:48:09 +0200

    🚧 Create prisma instance and query all the users in auth

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const prisma = require("../utils/db");
 
-router.route("/").get((req, res) => {
-  res.send("/auth router");
+router.route("/").get(async (req, res) => {
+  const users = await prisma.user.findMany();
+  console.log("users", users);
+  res.send("auth page");
 });
 
 module.exports = router;
