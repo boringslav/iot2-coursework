b9d48434f6b83bc0a004500e00c554115e60c414
Borislav Stoyanov <borislav.stoyanov@musala.com>
2022-12-24 16:11:30 +0200

    🔐 Prisma migration (require unique username) + SignUp functionality with CryptoJS encryption

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,11 +1,37 @@
 const express = require("express");
+const CryptoJS = require("crypto-js");
 const router = express.Router();
 const prisma = require("../utils/db");
 
-router.route("/").get(async (req, res) => {
-  const users = await prisma.user.findMany();
-  console.log("users", users);
-  res.send("auth page");
+router.route("/sign-up").post(async (req, res) => {
+  const { username, password, repeatedPassword } = req.body;
+
+  if (password !== repeatedPassword) {
+    return res
+      .status(401)
+      .send(JSON.stringify({ message: "Password don't match!" }));
+  }
+
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        password: CryptoJS.AES.encrypt(
+          password,
+          process.env.SECRET_KEY
+        ).toString(),
+      },
+      select: {
+        username: true,
+        id: true,
+      },
+    });
+
+    res.status(201).send(JSON.stringify(newUser));
+  } catch (e) {
+    console.log(e);
+    res.status(401).send(JSON.stringify({ message: "User already exists!" }));
+  }
 });
 
 module.exports = router;
