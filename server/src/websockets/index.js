08e2eac5bf019c4ce7c7b6ef6813e16bdd34935f
Borislav Stoyanov <borislav.stoyanov@musala.com>
2022-12-25 15:20:33 +0200

    ✨ Write websocket messages to json file

--- server/src/websockets/index.js
+++ server/src/websockets/index.js
@@ -1,9 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+const reportFilePath = path.join(`${__dirname}/report.json`);
+
 module.exports = (ws) => {
   console.info("A new user connected!");
   ws.send("The device is successfully connected!");
 
   ws.on("message", (message) => {
+    let jsonFileData = JSON.parse(fs.readFileSync(reportFilePath));
     const devicesData = JSON.parse(message);
-    console.info(devicesData);
+    console.log("Devices Data: ", devicesData);
+    jsonFileData[Date.now()] = devicesData;
+    fs.writeFileSync(reportFilePath, JSON.stringify(jsonFileData));
+  });
+
+  ws.on("error", (e) => {
+    console.error("Websocket error", e);
   });
 };
